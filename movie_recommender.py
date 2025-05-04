46a9c4ab879754799dfa90adfca0fe159f876053
Jose A. Pinell <japinell@yahoo.com>
2025-05-04 12:22:03 -0700

    Initial commit

---
+++ movie_recommender.py
@@ -0,0 +1,4 @@
+# Movie Recommendation System
+# This script uses the framework provided by Bernd Schrooten in Building a Movie Recommendation System published by Packt Publishing.
+import pandas as pd
+import matplotlib.pyplot as plt
